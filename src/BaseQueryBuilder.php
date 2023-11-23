6ff753aca2674a0f26d1b322a9b228d7ea26e9d8
D2MY <vania.klimovich@gmail.com>
2023-11-23 21:37:23 +0300

    save

--- src/BaseQueryBuilder.php
+++ src/BaseQueryBuilder.php
@@ -2,11 +2,32 @@
 
 namespace D2my\Builders;
 
-use Illuminate\Database\Query\Builder;
+use Closure;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
-class BaseQueryBuilder extends Builder
+class BaseQueryBuilder extends QueryBuilder
 {
     use BuilderTrait;
+
+    /**
+     * @param string $column
+     * @param array|int|string|null $value
+     * @param string $expression
+     * @return Closure|null
+     */
+    function normalizedWhere(string $column, array|int|string|null $value, string $expression = '='): ?Closure
+    {
+        [$method, $args] = match (true) {
+            is_array($value) => ['whereIn', [$column, $value]],
+            is_null($value) => ['whereNull', [$column]],
+            default => ['where', [$column, $expression, $value]]
+        };
+
+        return function (EloquentBuilder|QueryBuilder $query) use ($args, $method) {
+            $query->$method(...$args);
+        };
+    }
 
     /**
      * @param $method
